7f05611ab3452f3206958225c93c8fda682a7495
guenchi <louis.deguenchy@gmail.com>
2018-10-28 02:40:20 +0200

    Update src/list.js

--- src/list.js
+++ src/list.js
@@ -3,24 +3,40 @@
 
 const cons = (x, y) => m => m(x, y);
 
+const list = (...x) => {
+    let lst = null;
+    for(let i = x.length; i >= 0; i--){
+        lst = cons(x[i], lst);
+    }
+    return lst;
+}
+
 const car = (m) => m((x, y) => x);
 
 const cdr = (m) => m((x, y) => y);
 
-const list = (...k) => {
-    let x = null;
-    for(let i = k.length; i >= 0; i--){
-        x = cons(k[i], x);
-    }
-    return x;
-}
+const caar = x => car(car(x));
+
+const cadr = x => car(cdr(x));
+
+const cdar = x => cdr(car(x));
+
+const cddr = x => cdr(cdr(x));
+
+const caaar = x => car(car(car(x)));
+
+const caadr = x => car(car(cdr(x)));
+
+const caddr = x => car(cdr(cdr(x)));
+
+const cdddr = x => cdr(cdr(cdr(x)));
 
 const ref = (lst, k) => {
     if(lst === null){
         return false;
     }else{
-        if(car(car(lst)) === k){
-            return cdr(car(lst));
+        if(caar(lst) === k){
+            return cdar(lst);
         }else{
             return ref(cdr(lst), k);
         }
