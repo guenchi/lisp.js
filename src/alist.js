1015ea0ac8e6c374eaeb16254ac0405bd40969d5
guenchi <louis.deguenchy@gmail.com>
2018-10-29 02:26:27 +0100

    Update src/alist.js

--- src/alist.js
+++ src/alist.js
@@ -1,13 +1,50 @@
 
 'use strict'
 
-const {cons, car, cdr, caar, cdar, cddr} = require('./list');
+const {cons, car, cdr, caar, cdar, cddr, setcar} = require('./list');
 
-const ref = (l, x) => l ? caar(l) === x ? cdar(l) : ref(cdr(l), x) : false;
+const ref = (l, x) =>{
+    if(l){
+        if(caar(l) === x){
+            return cdar(l);
+        }else{
+            return ref(cdr(l), x);
+        }
+    }else{
+        return false;
+    }
+}
 
-const val = (l, x) => l ? cdar(l) === x ? caar(l) : val(cdr(l), x) : false;
+
+const val = (l, x) =>{
+    if(l){
+        if(cdar(l) === x){
+            return caar(l);
+        }else{
+            return val(cdr(l), x);
+        }
+    }else{
+        return false;
+    }
+}
 
 
+const alter = (l, x, y) =>{
+    if(car(l)){
+        if(caar(l) === x){
+            setcar(l, cons(x, y));
+        }else{
+            if(cdar(l)){
+                alter(cdr(l), x, y);
+            }else{
+                return false;
+            }
+        }
+    }else{
+        return false;
+    }
+}
 
 exports.ref = ref;
 exports.val = val;
+exports.alter = alter;
